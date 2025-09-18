b963a7abf23b8eef5eb673333df28f335f945ceb
trippedBit <130838437+trippedBit@users.noreply.github.com>
2025-09-18 17:34:05 +0200

    test: linking requirement and adding test for default pin numbers

--- tests/chassis_motor.cpp
+++ tests/chassis_motor.cpp
@@ -2,12 +2,14 @@
 
 #include "../src/chassis_motor.h"
 
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/15
 TEST_CASE("Chassis motor can be created", "[chassis_motor]")
 {
     ChassisMotor motor(5, 6, 7);
     REQUIRE(true); // If we reach this point, the motor was created successfully
 }
 
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/15
 TEST_CASE("Chassis motor can set movement direction", "[chassis_motor]")
 {
     ChassisMotor motor(5, 6, 7);
@@ -35,4 +37,16 @@ TEST_CASE("Chassis motor can set movement direction", "[chassis_motor]")
         int result = motor.setMovementDirection(static_cast<ChassisMotor::MovementDirection>(999));
         REQUIRE(result == -2); // Expect error code for invalid direction
     }
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/15
+TEST_CASE("Chassis motor default pin configuration", "[chassis_motor]")
+{
+    REQUIRE(ENA_PIN == 0);
+    REQUIRE(FORWARD1_PIN == 12);
+    REQUIRE(BACKWARD1_PIN == 32);
+
+    REQUIRE(ENB_PIN == 4);
+    REQUIRE(FORWARD2_PIN == 16);
+    REQUIRE(BACKWARD2_PIN == 17);
 }
