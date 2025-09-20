dd7c706bccf0957f88e47a9780dae2b710484b53
trippedBit <130838437+trippedBit@users.noreply.github.com>
2025-09-20 08:37:28 +0200

    test: add negative tests

--- tests/self_check.cpp
+++ tests/self_check.cpp
@@ -13,3 +13,30 @@ TEST_CASE("Self-check function exists and returns true", "[self_check]")
                             rightMotor);
     REQUIRE(result == true);
 }
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/18
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/19
+TEST_CASE("Self-check function fails due to left motor check", "[self_check]")
+{
+    ChassisMotor leftMotor(5, 6, 7);
+    ChassisMotor rightMotor(8, 9, 10);
+    // Force failed check
+    bool result = selfCheck(leftMotor,
+                            rightMotor,
+                            true);
+    REQUIRE(result == false);
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/18
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/19
+TEST_CASE("Self-check function fails due to right motor check", "[self_check]")
+{
+    ChassisMotor leftMotor(5, 6, 7);
+    ChassisMotor rightMotor(8, 9, 10);
+    // Force failed check
+    bool result = selfCheck(leftMotor,
+                            rightMotor,
+                            false,
+                            true);
+    REQUIRE(result == false);
+}
