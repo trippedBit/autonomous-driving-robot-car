17ebfea51f6940ca99d4b659d36738d6a3ed178f
trippedBit <130838437+trippedBit@users.noreply.github.com>
2025-09-26 15:00:37 +0200

    test: add tests for PWM factor

--- tests/chassis_motor.cpp
+++ tests/chassis_motor.cpp
@@ -80,3 +80,57 @@ TEST_CASE("Chassis motor velocities", "[chassis_motor]")
     REQUIRE(VELOCITY_MID == 200);
     REQUIRE(VELOCITY_MAX == 250);
 }
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor initialization with default PWM factor", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7);
+    REQUIRE(true);
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor initialization with non-default PWM factor", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7, 0.9);
+    REQUIRE(true);
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor calculated PWM with default PWM factor", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7);
+    float result = motor.setVelocityPWM(200);
+    REQUIRE(result == 200.0f); // Default factor is 1, so PWM should be unchanged
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor calculated PWM with non-default PWM factor", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7, 0.9);
+    float result = motor.setVelocityPWM(200);
+    REQUIRE(result == 180.0f);
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor calculated PWM above upper limit", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7, 2);
+    float result = motor.setVelocityPWM(200);
+    REQUIRE(result == 255.0f); // Calculated PWM is limited to range 0-255
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor calculated PWM below lower limit", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7, -2);
+    float result = motor.setVelocityPWM(200);
+    REQUIRE(result == 0.0f); // Calculated PWM is limited to range 0-255
+}
+
+// Requirement: https://github.com/trippedBit/autonomous-driving-robot-car/issues/36
+TEST_CASE("Chassis motor calculated PWM with factor zero", "[chassis_motor]")
+{
+    ChassisMotor motor(5, 6, 7, 0);
+    float result = motor.setVelocityPWM(200);
+    REQUIRE(result == 0.0f); // Calculated PWM is limited to range 0-255
+}
