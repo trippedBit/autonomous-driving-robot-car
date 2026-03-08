058ba86cf7a2592dca5e223a754371733aa44cea
trippedBit <130838437+trippedBit@users.noreply.github.com>
2026-03-08 17:36:39 +0100

    test: temporarily removed tests, reimplementation needed based on requirements

--- tests/wifi_wrapper.cpp
+++ tests/wifi_wrapper.cpp
@@ -2,27 +2,3 @@
 
 #include "../src/configuration.h"
 #include "../src/wifi_wrapper.h"
-
-// Requirement: None
-TEST_CASE("WiFi initialization", "[wifi_wrapper]")
-{
-    WiFiWrapper wifiWrapper(WIFI_NAME,
-                            WIFI_PASSWORD,
-                            WIFI_IP,
-                            WIFI_DNS,
-                            WIFI_GATEWAY,
-                            WIFI_SUBNET);
-    REQUIRE(true);
-}
-
-TEST_CASE("connectWiFi() dummy call", "[wifi_wrapper]")
-{
-    WiFiWrapper wifiWrapper(WIFI_NAME,
-                            WIFI_PASSWORD,
-                            WIFI_IP,
-                            WIFI_DNS,
-                            WIFI_GATEWAY,
-                            WIFI_SUBNET);
-    wifiWrapper.connectWiFi();
-    REQUIRE(true);
-}
